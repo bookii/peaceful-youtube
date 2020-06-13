59e4faa9b42f9ab3f549e41da9b2be723785aa61
bookii <bookii.dev@gmail.com>
2020-06-13 13:01:54 +0900

    Make linter work property / Set GitHub Actions

--- contentScript.js
+++ contentScript.js
@@ -2,23 +2,25 @@
 addEventListener("load", removeDislikeButton, false);
 
 // ページ内遷移イベント
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.type === "onUpdated") {
-        removeDislikeButton()
-    }
-})
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.type === "onUpdated") {
+    removeDislikeButton();
+  }
+});
 
 function removeDislikeButton() {
-    const jsLoaded = () => {
-        console.log("try")
-        const buttons = document.getElementById("top-level-buttons")?.getElementsByTagName("ytd-toggle-button-renderer");
-        if (buttons != null && buttons.length >= 2) {
-            buttons[1].remove();
-            // sentiment が残っている場合は削除する
-            document.getElementById("sentiment")?.remove();
-            clearInterval(jsInitCheckTimer);
-        }
+  const jsLoaded = () => {
+    console.log("try");
+    const buttons = document
+      .getElementById("top-level-buttons")
+      ?.getElementsByTagName("ytd-toggle-button-renderer");
+    if (buttons != null && buttons.length >= 2) {
+      buttons[1].remove();
+      // sentiment が残っている場合は削除する
+      document.getElementById("sentiment")?.remove();
+      clearInterval(jsInitCheckTimer);
     }
-    // delay の値はお好みで
-    const jsInitCheckTimer = setInterval(jsLoaded, 200);
+  };
+  // delay の値はお好みで
+  const jsInitCheckTimer = setInterval(jsLoaded, 200);
 }
